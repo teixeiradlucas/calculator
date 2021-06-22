067e2545e0071053d56a8574aea2dca5baf687e6
Lucas Francisco Teixeira <>
2021-06-21 21:00:07 -0300

    fix

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -3,43 +3,50 @@ import 'button.dart';
 import 'button_row.dart';
 
 class Keyboard extends StatelessWidget {
+  final void Function(String) cb;
+
+  Keyboard(this.cb);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 500,
       child: Column(
-        children:<Widget> [
+        children: <Widget>[
           ButtonRow([
-            Button.big(text: 'C'),
-            Button(text: '%'),
-            Button(text: '/'),
+            Button.big(text: 'C', color: Button.DARK, cb: cb),
+            Button(text: '%', color: Button.DARK, cb: cb),
+            Button.operation(text: '/', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
-            Button(text: '7'),
-            Button(text: '8'),
-            Button(text: '9'),
-            Button(text: 'x'),
+            Button(text: '7', cb: cb),
+            Button(text: '8', cb: cb),
+            Button(text: '9', cb: cb),
+            Button.operation(text: 'x', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
-            Button(text: '4'),
-            Button(text: '5'),
-            Button(text: '6'),
-            Button(text: '-'),
+            Button(text: '4', cb: cb),
+            Button(text: '5', cb: cb),
+            Button(text: '6', cb: cb),
+            Button.operation(text: '-', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
-            Button(text: '1'),
-            Button(text: '2'),
-            Button(text: '3'),
-            Button(text: '+'),
+            Button(text: '1', cb: cb),
+            Button(text: '2', cb: cb),
+            Button(text: '3', cb: cb),
+            Button.operation(text: '+', cb: cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
-            Button.big(text: '0'),
-            Button(text: ','),
-            Button(text: '='),
+            Button.big(text: '0', cb: cb),
+            Button(text: ',', cb: cb),
+            Button.operation(text: '=', cb: cb),
           ]),
         ],
       ),
-      
     );
   }
 }
